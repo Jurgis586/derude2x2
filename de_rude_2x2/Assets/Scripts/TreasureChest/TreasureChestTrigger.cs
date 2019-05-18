1b3612016d5c4a4a6fcbdbdbdda71ebd04fbfac9
Jurgis <jurgis586@gmail.com>
2019-05-18 19:59:52 +0300

    Treasure Chest gives scorePickup on colide

---
+++ de_rude_2x2/Assets/Scripts/TreasureChest/TreasureChestTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureChestTrigger : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject treasureChest;
+    public GameObject treasureChestPickup;
+    private float inactiveTime;
+
+    private void Start()
+    {
+        treasureChestPickup.SetActive(false);
+        inactiveTime = Time.time;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (Time.time - inactiveTime > 30)
+        {
+            inactiveTime = Time.time;
+            treasureChestPickup.SetActive(true);
+        }
+    }
+}
